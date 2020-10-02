c57ad94d5310877bade3d5e1b351a415c99fa783
Francis Silveira Andrade <francis.andrade@objective.com.br>
2020-10-01 21:18:34 -0300

    Aula 09

--- CL.WebApi/Configuration/SwaggerConfig.cs
+++ CL.WebApi/Configuration/SwaggerConfig.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
 using System;
 using System.IO;
 using System.Reflection;
@@ -32,6 +33,8 @@ namespace CL.WebApi.Configuration
                         },
                         TermsOfService = new Uri("https://opensource.org/osd")
                     });
+
+                c.AddFluentValidationRules();
 
                 var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
